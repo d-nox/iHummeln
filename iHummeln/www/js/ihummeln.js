bfbbc93d8d52c1ea1207809e1c46b02ddb3f2f45
Daniel Dominkovic <danieldominkovic@Daniels-MacBook-Pro.local>
2016-02-18 20:12:44 +0100

    angular integration, identifizierte hummeln

--- iHummeln/www/js/ihummeln.js
+++ iHummeln/www/js/ihummeln.js
@@ -1,12 +1,50 @@
 //--------------------------------------------------------------------------------------------------------------------------------------------------App
 var iHummelApp = angular.module('iHummelApp', [
-    'iHummelControllers'
+    'iHummelControllers',
+    'ngStorage'
 ]);
 //-------------------------------------------------------------------------------------------------------------------------------------------Controller
 var iHummelControllers = angular.module('iHummelControllers', []);
 
-iHummelControllers.controller('mainController', ['$scope',
-    function ($scope){
-        console.log('Angular Loaded');
-    }
-])
+iHummelControllers.controller('mainController', ['$scope', '$localStorage',
+    function ($scope, $localStorage) {
+        $scope.hummeln = $localStorage.hummeln;
+
+        $scope.delete = function (id) {
+            var hummeln = $localStorage.hummeln;
+            var hummelnNew = [];
+            for (i = 0; i < hummeln.length; i++) {
+                if (hummeln[i].Id !== id) {
+                    hummelnNew.push(hummeln[i]);
+                }
+            }
+            $localStorage.hummeln = hummelnNew;
+        };
+
+        $scope.add = function (id, name, path) {
+            if ($localStorage.hummeln === undefined || $localStorage.idCount === undefined) {
+                $localStorage.idCount = 0;
+                $localStorage.hummeln = [];
+            }
+            var newHummel = {
+                Id: $localStorage.idCount+"",
+                hummelId: id+"",
+                name: name+"",
+                picPath: path,
+                longitude: "13.134134134",
+                latitude: "23.214314143",
+                timestamp: "134134134"
+            };
+            if ($localStorage.hummeln.length === 0) {
+                var hummelnNew = [];
+                hummelnNew.push(newHummel);
+            } else {
+                var hummelnNew = $localStorage.hummeln;
+                hummelnNew.push(newHummel);
+                console.log(JSON.stringify(newHummel));
+            }
+
+            $localStorage.hummeln = hummelnNew;
+            $localStorage.idCount++;
+        };
+    }]);
