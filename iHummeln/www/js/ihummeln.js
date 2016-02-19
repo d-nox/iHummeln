87fbc6a5e645585e008365bd1a1a4232f4accb8f
Daniel Dominkovic <danieldominkovic@Daniels-MacBook-Pro.local>
2016-02-19 15:54:28 +0100

    so viele commitments -.-

--- iHummeln/www/js/ihummeln.js
+++ iHummeln/www/js/ihummeln.js
@@ -6,51 +6,53 @@ var iHummelApp = angular.module('iHummelApp', [
 //-------------------------------------------------------------------------------------------------------------------------------------------Controller
 var iHummelControllers = angular.module('iHummelControllers', []);
 
-iHummelControllers.controller('mainController', ['$scope', '$localStorage',
-    function ($scope, $localStorage) {
-        $scope.hummeln = $localStorage.hummeln;
+iHummelControllers.controller('mainController', ['$scope', '$window',
+    function ($scope, $localStorage, $window) {
+        if (localStorage['hummeln'] !== undefined) {
+            $scope.hummeln = JSON.parse(localStorage['hummeln']);
+        }
 
         $scope.delete = function (id) {
-            var hummeln = $localStorage.hummeln;
+            var hummeln = JSON.parse(localStorage['hummeln']);
             var hummelnNew = [];
             for (i = 0; i < hummeln.length; i++) {
                 if (hummeln[i].Id !== id) {
                     hummelnNew.push(hummeln[i]);
                 }
             }
-            $localStorage.hummeln = hummelnNew;
+            localStorage['hummeln'] = JSON.stringify(hummelnNew);
             location.reload();
         };
 
         $scope.add = function (id, name, path) {
-            if ($localStorage.hummeln === undefined || $localStorage.idCount === undefined) {
-                $localStorage.idCount = 0;
-                $localStorage.hummeln = [];
+            if (localStorage['hummeln'] === undefined || localStorage['idCount'] === undefined) {
+                localStorage['idCount'] = 0;
+                localStorage['hummeln'] = [];
             }
             var newHummel = {
-                Id: $localStorage.idCount+"",
-                hummelId: id+"",
-                name: name+"",
+                Id: localStorage['idCount'] + "",
+                hummelId: id + "",
+                name: name + "",
                 picPath: path,
                 longitude: "13.134134134",
                 latitude: "23.214314143",
                 timestamp: "134134134"
             };
-            if ($localStorage.hummeln.length === 0) {
+            if (localStorage['hummeln'].length === 0) {
                 var hummelnNew = [];
                 hummelnNew.push(newHummel);
             } else {
-                var hummelnNew = $localStorage.hummeln;
+                var hummelnNew = JSON.parse(localStorage['hummeln']);
                 hummelnNew.push(newHummel);
             }
 
-            $localStorage.hummeln = hummelnNew;
-            $localStorage.idCount++;
+            localStorage['hummeln'] = JSON.stringify(hummelnNew);
+            localStorage['idCount'] = parseInt(localStorage['idCount']) + 1;
+            //window.location = '/';
             location.reload();
-            window.location = '/';
         };
 
-        $scope.clearAll = function (){
+        $scope.clearAll = function () {
             localStorage.clear();
             location.reload();
         };
